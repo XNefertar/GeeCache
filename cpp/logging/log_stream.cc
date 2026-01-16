00c5431f1f195c52551a30698a30602cd949a529
XNefertar <xnefertar@gmail.com>
2026-01-16 15:02:41 +0800

    优化LogStream类中的代码格式，简化函数体，提升可读性

--- cpp/logging/log_stream.cc
+++ cpp/logging/log_stream.cc
@@ -5,27 +5,23 @@
 #include <cstdio>
 #include <cstdint>
 
-namespace lsm
-{
+namespace lsm {
 
     const char digits[] = "9876543210123456789";
     const char *zero = digits + 9;
 
     template <typename T>
-    size_t convert(char buf[], T value)
-    {
+    size_t convert(char buf[], T value) {
         T i = value;
         char *p = buf;
 
-        do
-        {
+        do {
             int lsd = static_cast<int>(i % 10);
             i /= 10;
             *p++ = zero[lsd];
         } while (i != 0);
 
-        if (value < 0)
-        {
+        if (value < 0) {
             *p++ = '-';
         }
         *p = '\0';
@@ -34,68 +30,56 @@ namespace lsm
     }
 
     template <typename T>
-    void LogStream::formatInteger(T v)
-    {
-        if (_buffer.avail() >= kMaxNumericSize)
-        {
+    void LogStream::formatInteger(T v) {
+        if (_buffer.avail() >= kMaxNumericSize) {
             size_t len = convert(_buffer.current(), v);
             _buffer.add(len);
         }
     }
 
-    LogStream &LogStream::operator<<(short v)
-    {
+    LogStream &LogStream::operator<<(short v) {
         *this << static_cast<int>(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(unsigned short v)
-    {
+    LogStream &LogStream::operator<<(unsigned short v) {
         *this << static_cast<unsigned int>(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(int v)
-    {
+    LogStream &LogStream::operator<<(int v) {
         formatInteger(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(unsigned int v)
-    {
+    LogStream &LogStream::operator<<(unsigned int v) {
         formatInteger(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(long v)
-    {
+    LogStream &LogStream::operator<<(long v) {
         formatInteger(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(unsigned long v)
-    {
+    LogStream &LogStream::operator<<(unsigned long v) {
         formatInteger(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(long long v)
-    {
+    LogStream &LogStream::operator<<(long long v) {
         formatInteger(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(unsigned long long v)
-    {
+    LogStream &LogStream::operator<<(unsigned long long v) {
         formatInteger(v);
         return *this;
     }
 
-    LogStream &LogStream::operator<<(const void *p)
-    {
+    LogStream &LogStream::operator<<(const void *p) {
         uintptr_t v = reinterpret_cast<uintptr_t>(p);
-        if (_buffer.avail() >= kMaxNumericSize)
-        {
+        if (_buffer.avail() >= kMaxNumericSize) {
             char *buf = _buffer.current();
             buf[0] = '0';
             buf[1] = 'x';
@@ -107,10 +91,8 @@ namespace lsm
         return *this;
     }
 
-    LogStream &LogStream::operator<<(double v)
-    {
-        if (_buffer.avail() >= kMaxNumericSize)
-        {
+    LogStream &LogStream::operator<<(double v) {
+        if (_buffer.avail() >= kMaxNumericSize) {
             int len = snprintf(_buffer.current(), kMaxNumericSize, "%.12g", v);
             _buffer.add(len);
         }
